bd3da61e3df283271acc1835907b64778b6ac3e6
tessc14 <tessc14@gmail.com>
2023-03-22 10:23:35 +0000

    remove duplicate ProviderCard

--- src/Components/index.js
+++ src/Components/index.js
@@ -2,6 +2,5 @@
 export { default as ProviderCard } from './ProviderCard';
 export { default as PetProfileForm } from './PetProfileForm'
 export { default as CardProfile  } from './PetProfileForm'
-export { default as ProviderCard } from './ProviderCard';
 export {default as Filters} from './Filters'
 
