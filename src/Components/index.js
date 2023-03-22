0f91993b32a76a3a6d7fcb18ac8a53601edd6cb2
tessc14 <tessc14@gmail.com>
2023-03-22 10:21:45 +0000

    change index.js to index.jsx in components exports

--- src/Components/index.js
+++ src/Components/index.js
@@ -2,4 +2,6 @@
 export { default as ProviderCard } from './ProviderCard';
 export { default as PetProfileForm } from './PetProfileForm'
 export { default as CardProfile  } from './PetProfileForm'
+export { default as ProviderCard } from './ProviderCard';
+export {default as Filters} from './Filters'
 
